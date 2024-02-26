3b942a62798608d1591f23185d81287836184eef
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-26 15:10:19 +0600

    design ready

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+// button selecting
 const allbtns = document.querySelectorAll('.btn');
+// selecting the reset button
+const resetBtn = document.getElementById('reset');
+// result display 
+const resultView = document.getElementById('valueDisplay');
+// rotation image
+const rotateImage = document.getElementById('rotimg');
 
-
-// allbtns.forEach((btn, index) => {
-//     btn.addEventListener('click', function() {
-//         const degree = parseInt(btn.querySelector('span').innerText); // Extract degree from button
-//         image.style.transform = `rotate(${degree}deg)`; // Rotate image
-//     });
-
-allbtns.forEach((btn, index) => {
-    btn.addEventListener('click', function() {
+for(const btn of allbtns){
+    btn.addEventListener('click', function(){
+        resultView.classList.remove('hidden');
         const degree = parseInt(btn.querySelector('span').innerText);
-        image.style.transform = `rotate(${degree}deg)`;
-    });
+        resultView.innerText= `The Rotation Of ${degree} Degree`;
+        resultView.classList.add('bg-[#ffeded]')
+        rotateImage.style.transform = `rotate(${degree}deg)`
+    })
+}
+resetBtn.addEventListener('click', function(){
+    resultView.classList.add('hidden')
+    rotateImage.style.transform = 'rotate(0deg)'
+})
